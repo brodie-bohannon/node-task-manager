f33d7f99361cbf82dbaddcb35104520d28870aca
Joshua Osburn <joshuakosburn@gmail.com>
2020-05-24 21:19:56 -0700

    Initial commit

---
+++ src/models/user.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"!')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number!')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: false
+        }
+    }]
+})
+
+userSchema.methods.generateAuthToken = async function () {
+    //Generate Token
+    const token = jwt.sign({_id: this._id.toString()}, 'thisisrandom')
+    //Save the token to the User Document (MongoDB)
+    this.tokens = this.tokens.concat({ token })
+    //Save the User
+    await this.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('Unable to login!')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) {
+        throw new Error('Unable to login!')
+    }
+
+    return user
+}
+
+//HASH PLAINTEXT PASSWORD
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
---
+++ src/routers/user.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+//CREATE A NEW USER
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//AUTHENTICATE USER
+router.post('/users/login', async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+    try {
+        const user = await User.findByCredentials(email, password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+//GET ALL USERS
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+//GET A USER BY ID
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findById(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+//UPDATE A USER
+router.patch('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['firstName', 'lastName', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findById(_id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+//DELETE A USER
+router.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
