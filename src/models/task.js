3e4315eb81a1bf11a97b03858dc076ede1602bd5
Joshua Osburn <joshuakosburn@gmail.com>
2020-05-27 21:05:39 -0700

    Added sorting and pagination functionality for tasks

--- src/models/task.js
+++ src/models/task.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const Task = mongoose.model('Task', {
+const taskSchema = new mongoose.Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
@@ -15,6 +15,10 @@ const Task = mongoose.model('Task', {
         type: Boolean,
         default: false
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 module.exports = Task
--- src/routers/task.js
+++ src/routers/task.js
@@ -16,15 +16,32 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit==5&skip=10
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+
     try {
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                // parseInt is provided by JS. Pass in the limit parameter (which is a string) to be parsed
+                // as an int to return that many results and 1 time
+                limit: parseInt(req.query.limit)
+            }
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
 })
 
+//GET A SPECIFIC TASK
 router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
@@ -40,6 +57,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
+//UPDATE A TASK
 router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
@@ -64,6 +82,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     }
 })
 
+//DELETE A TASK
 router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
