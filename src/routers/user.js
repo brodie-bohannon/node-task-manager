80998bd63dc757052a5164f0ae722290f6b3ac2e
Joshua Osburn <joshuakosburn@gmail.com>
2020-05-28 21:25:18 -0700

    Added express error handlers for errors caught by Multer middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -92,12 +92,22 @@ const upload = multer({
     dest: 'images',
     limits: {
         fileSize: 2000000 //2MBs
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg|jpeg|gif)$/)) {
+            return cb(new Error('Please upload an image!'))
+        }
+
+        cb(undefined, true)
     }
 })
+
 
 //USER UPLOADS PROFILE PIC
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
